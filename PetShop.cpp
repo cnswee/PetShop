30e967861fcae4518818f85cfb7ab94c6d30fa18
Swee <cnswee@qq.com>
2020-11-27 19:48:08 +0800

    .

---
+++ PetShop.cpp
@@ -0,0 +1,454 @@
+#include <iostream>
+#include <string>
+#include "PetShop.h"
+#include <stdlib.h>
+using namespace std;
+
+Shop QMPets;
+
+void ClearScreen()
+{
+    if (system("CLS"))
+        system("clear");
+}
+
+void SearchByType()
+{
+    cout << "\nPlease enter the type(Bird/Cat/Dog):" << endl;
+    string type;
+    cin >> type;
+    while (type != "Bird" && type != "Cat" && type != "Dog")
+    {
+        cout << "\nNo such type,please retry:" << endl;
+        cin >> type;
+    }
+    QMPets.SearchByType(type);
+    system("Pause");
+}
+
+void SearchById()
+{
+    cout << "\nPlease enter the id:" << endl;
+    string id;
+    cin >> id;
+    if (!QMPets.SearchById(id))
+        cout << "There is no such pet." << endl;
+    system("Pause");
+}
+
+void SearchByPrice()
+{
+    cout << "\nPlease enter the min:$" << endl;
+    string min;
+    cin >> min;
+    cout << "\nPlease enter the max:$" << endl;
+    string max;
+    cin >> max;
+    QMPets.SearchByPrice(min, max);
+    system("Pause");
+}
+
+void SearchByDetails()
+{
+    cout << "\nPlease enter the type(Bird/Cat/Dog):" << endl;
+    string type;
+    cin >> type;
+    while (type != "Bird" && type != "Cat" && type != "Dog")
+    {
+        cout << "\nNo such type,please retry:" << endl;
+        cin >> type;
+    }
+    if (type == "Bird")
+    {
+        cout << "\nPlease enter that if they are licenced(yes/no):" << endl;
+        string slicenced;
+        cin >> slicenced;
+        bool licenced;
+        if (slicenced == "yes")
+            licenced = true;
+        else if (slicenced == "no")
+            licenced = false;
+        QMPets.SearchByDetail("Bird", licenced);
+        system("Pause");
+    }
+    else
+    {
+        cout << "\nPlease enter that if they are neutered(yes/no):" << endl;
+        bool neutered;
+        string sneutered;
+        cin >> sneutered;
+        if (sneutered == "yes")
+            neutered = true;
+        else if (sneutered == "no")
+            neutered = false;
+        cout << "\nPlease enter that if they are vaccinated(yes/no):" << endl;
+        bool vaccinated;
+        string svaccinated;
+        cin >> svaccinated;
+        if (svaccinated == "yes")
+            vaccinated = true;
+        else if (svaccinated == "no")
+            vaccinated = false;
+        QMPets.SearchByDetail(type, neutered, vaccinated);
+        system("Pause");
+    }
+}
+
+void SearchPets()
+{
+    ClearScreen();
+    cout << "-------------------------------------\n"
+         << "           QM Pets        \n"
+         << "-------------------------------------" << endl;
+    cout << "      Option    Function         \n"
+         << "-------------------------------------" << endl;
+    cout << "      1         Search by type    " << endl;
+    cout << "      2         Search by id       " << endl;
+    cout << "      3         Search by price      " << endl;
+    cout << "      4         Search by details    " << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "Please enter the option:" << endl;
+    string option;
+    cin >> option;
+    while (atoi(option.c_str()) < 1 || atoi(option.c_str()) > 4)
+    {
+        cout << "\nNo such option,please retry:" << endl;
+        cin >> option;
+    }
+    switch (atoi(option.c_str()))
+    {
+    case 1:
+        SearchByType();
+        break;
+    case 2:
+        SearchById();
+        break;
+    case 3:
+        SearchByPrice();
+        break;
+    case 4:
+        SearchByDetails();
+        break;
+    }
+}
+
+void DisplayAll()
+{
+    ClearScreen();
+    QMPets.DisplayAll();
+    system("Pause");
+}
+
+void AddPets()
+{
+    ClearScreen();
+    cout << "Please enter the type(Bird/Cat/Dog):" << endl;
+    string type;
+    cin >> type;
+    while (type != "Bird" && type != "Cat" && type != "Dog")
+    {
+        cout << "\nNo such type,please retry:" << endl;
+        cin >> type;
+    }
+    cout << "\nPlease enter the store id:" << endl;
+    string id;
+    cin >> id;
+    while (QMPets.SearchById(id))
+    {
+        cout << "There is a same id.Please retry:" << endl;
+        cin >> id;
+    }
+    cout << "\nPlease enter the breed:" << endl;
+    string breed;
+    cin >> breed;
+    cout << "\nPlease enter the age:" << endl;
+    string age;
+    cin >> age;
+    cout << "\nPlease enter the colour:" << endl;
+    string colour;
+    cin >> colour;
+    cout << "\nPlease enter the origin:" << endl;
+    string origin;
+    cin >> origin;
+    cout << "\nPlease enter the price($):" << endl;
+    string price;
+    cin >> price;
+    if (type == "Bird")
+    {
+        cout << "\nPlease enter that if they are licenced(yes/no):" << endl;
+        string slicenced;
+        cin >> slicenced;
+        bool licenced;
+        if (slicenced == "yes")
+            licenced = true;
+        else if (slicenced == "no")
+            licenced = false;
+        Pet *p = new Bird(id, breed, age, colour, origin, price, licenced); //p will be push_back so can't be deleted here.
+        QMPets.AddPets(p);
+    }
+    else if (type == "Cat")
+    {
+        cout << "\nPlease enter that if they are neutered(yes/no):" << endl;
+        string sneutered;
+        bool neutered;
+        cin >> sneutered;
+        if (sneutered == "yes")
+            neutered = true;
+        else if (sneutered == "no")
+            neutered = false;
+        cout << "\nPlease enter that if they are vaccinaed(yes/no):" << endl;
+        bool vaccinaed;
+        string svaccinaed;
+        cin >> svaccinaed;
+        if (svaccinaed == "yes")
+            vaccinaed = true;
+        else if (svaccinaed == "no")
+            vaccinaed = false;
+        Pet *p = new Cat(id, breed, age, colour, origin, price, neutered, vaccinaed);
+        QMPets.AddPets(p);
+    }
+    else if (type == "Dog")
+    {
+        cout << "\nPlease enter that if they are neutered(yes/no):" << endl;
+        string sneutered;
+        bool neutered;
+        cin >> sneutered;
+        if (sneutered == "yes")
+            neutered = true;
+        else if (sneutered == "no")
+            neutered = false;
+        cout << "\nPlease enter that if they are vaccinaed(yes/no):" << endl;
+        bool vaccinaed;
+        string svaccinaed;
+        cin >> svaccinaed;
+        if (svaccinaed == "yes")
+            vaccinaed = true;
+        else if (svaccinaed == "no")
+            vaccinaed = false;
+        Pet *p = new Dog(id, breed, age, colour, origin, price, neutered, vaccinaed);
+        QMPets.AddPets(p);
+    }
+    system("Pause");
+}
+
+void RemovePets()
+{
+    ClearScreen();
+    cout << "Please enter the store id to be removed:" << endl;
+    string id;
+    cin >> id;
+    if ((QMPets.RemovePets(id)) == 0)
+    {
+        cout << "There is no such pet." << endl;
+    }
+    system("Pause");
+}
+
+void ReadFile()
+{
+    ClearScreen();
+    QMPets.ReadPets();
+    cout << "Pets.dat is read." << endl;
+    system("Pause");
+}
+
+void SaveExit()
+{
+    ClearScreen();
+    QMPets.SavePets();
+    cout << "All is saved as Pets.dat.Good bye~" << endl;
+    system("Pause");
+    exit(0);
+}
+
+void DispalyFamilymenu()
+{
+    cout << "           QM Pets        \n"
+         << "-------------------------------------" << endl;
+    cout << "      Option    Function         \n"
+         << "-------------------------------------" << endl;
+    cout << "      1         Add family    " << endl;
+    cout << "      2         Show family      " << endl;
+    cout << "      3         Save as file     " << endl;
+    cout << "      4         Read from file    " << endl;
+    cout << "      5         Search baby   " << endl;
+    cout << "      6         Search mother   " << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "Please enter the option:" << endl;
+}
+
+void AddFamily()
+{
+    ClearScreen();
+    cout << "Please enter the baby store id :" << endl;
+    string id1;
+    cin >> id1;
+    while (!QMPets.SearchById(id1))
+    {
+        cout << "There is no such pet.Please retry:" << endl;
+        cin >> id1;
+    }
+    while (QMPets.SearchMother(id1))
+    {
+        cout << "It has a mother.Please retry:" << endl;
+        cin >> id1;
+    }
+    cout << "Please enter the mother store id :" << endl;
+    string id2;
+    cin >> id2;
+    while (!QMPets.SearchById(id2))
+    {
+        cout << "There is no such pet.Please retry:" << endl;
+        cin >> id2;
+    }
+    QMPets.AddFamily(id1, id2);
+    cout << "Family added." << endl;
+    system("Pause");
+}
+
+void ShowFamily()
+{
+    if (!QMPets.ShowFamily())
+        cout << "No family yet." << endl;
+    system("Pause");
+}
+
+void SaveFamily()
+{
+    QMPets.SaveFamily();
+    cout << "Saved as family.dat." << endl;
+    system("Pause");
+}
+
+void ReadFamily()
+{
+    QMPets.ReadFamily();
+    cout << "Read family.dat." << endl;
+    system("Pause");
+}
+
+void SearchBaby()
+{
+    cout << "Please enter the mother store id :" << endl;
+    string id;
+    cin >> id;
+    while (!QMPets.SearchById(id))
+    {
+        cout << "There is no such pet.Please retry:" << endl;
+        cin >> id;
+    }
+    if (!QMPets.SearchBaby(id))
+    {
+        cout << id << " has no baby." << endl;
+    };
+    system("Pause");
+}
+
+void SearchMother()
+{
+    cout << "Please enter the baby store id :" << endl;
+    string id;
+    cin >> id;
+    while (!QMPets.SearchById(id))
+    {
+        cout << "There is no such pet.Please retry:" << endl;
+        cin >> id;
+    }
+    if (!QMPets.SearchMother(id))
+    {
+        cout << id << " has no mother is this store." << endl;
+    };
+    system("Pause");
+}
+
+void FamilyFunction()
+{
+    ClearScreen();
+    DispalyFamilymenu();
+    string option;
+    cin >> option;
+    while (atoi(option.c_str()) < 1 || atoi(option.c_str()) > 7)
+    {
+        cout << "\nNo such option,please retry:" << endl;
+        cin >> option;
+    }
+    switch (atoi(option.c_str()))
+    {
+    case 1:
+        AddFamily();
+        break; //No need for delete family.
+    case 2:
+        ShowFamily();
+        break;
+    case 3:
+        SaveFamily();
+        break;
+    case 4:
+        ReadFamily();
+        break;
+    case 5:
+        SearchBaby();
+        break;
+    case 6:
+        SearchMother();
+        break;
+    }
+}
+
+void DisplayMainmenu()
+{
+    cout << "-------------------------------------\n"
+         << "           QM Pets        \n"
+         << "-------------------------------------" << endl;
+    cout << "      Option    Function         \n"
+         << "-------------------------------------" << endl;
+    cout << "      1         Search pets    " << endl;
+    cout << "      2         Display all       " << endl;
+    cout << "      3         Add pets      " << endl;
+    cout << "      4         Remove pets    " << endl;
+    cout << "      5         Read from file   " << endl;
+    cout << "      6         Save and exit   " << endl;
+    cout << "      7         More functions about family   " << endl;
+    cout << "-------------------------------------" << endl;
+    cout << "Please enter the option:" << endl;
+}
+
+int main()
+{
+    while (1)
+    {
+        ClearScreen();
+        DisplayMainmenu();
+        string option;
+        cin >> option;
+        while (atoi(option.c_str()) < 1 || atoi(option.c_str()) > 7)
+        {
+            cout << "\nNo such option,please retry:" << endl;
+            cin >> option;
+        }
+        switch (atoi(option.c_str()))
+        {
+        case 1:
+            SearchPets();
+            break;
+        case 2:
+            DisplayAll();
+            break;
+        case 3:
+            AddPets();
+            break;
+        case 4:
+            RemovePets();
+            break;
+        case 5:
+            ReadFile();
+            break;
+        case 6:
+            SaveExit();
+            break;
+        case 7:
+            FamilyFunction();
+            break;
+        }
+    }
+}
---
+++ PetShop.h
@@ -0,0 +1,507 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+class Pet
+{
+private:
+    string storeId;
+    string breed;
+    string age;
+    string colour;
+    string origin;
+    string price;
+    string motherId = " ";
+
+public:
+    Pet() {}
+
+    Pet(const string &storeId, const string &breed, const string &age, const string &colour, const string &origin, const string &price) : storeId(storeId), breed(breed), age(age), colour(colour), origin(origin), price(price)
+    {
+        // cout << "The pet is created" << endl;
+    }
+
+    virtual string Type()
+    {
+        return "Pet";
+    }
+
+    string GetStoreId()
+    {
+        return storeId;
+    }
+
+    string GetBreed()
+    {
+        return breed;
+    }
+
+    string GetAge()
+    {
+        return age;
+    }
+
+    string GetColour()
+    {
+        return colour;
+    }
+
+    string GetOrigin()
+    {
+        return origin;
+    }
+
+    string GetPrice()
+    {
+        return price;
+    }
+
+    void SetMotherId(string id)
+    {
+        motherId = id;
+    }
+
+    string GetMotherId()
+    {
+        return motherId;
+    }
+
+    virtual bool GetNeutered() {} //虚函数必须要实现，否则改为纯虚函数
+
+    virtual bool GetVaccinaed() {}
+
+    virtual bool GetLicenced() {}
+
+    virtual void Show() {}
+};
+
+class Cat : public Pet
+{
+private:
+    bool neutered;
+    bool vaccinaed;
+
+public:
+    Cat(const string &storeId, const string &breed, const string &age, const string &colour, const string &origin, const string &price, bool neutered, bool vaccinaed) : Pet(storeId, breed, age, colour, origin, price), neutered(neutered), vaccinaed(vaccinaed)
+    {
+        cout << "The cat is created" << endl;
+    }
+
+    string Type()
+    {
+        return "Cat";
+    }
+
+    void Show()
+    {
+        cout << Type() << " "
+             << "storeId:" << GetStoreId() << " "
+             << "breed:" << GetBreed() << " "
+             << "age:" << GetAge() << " "
+             << "colour:" << GetColour() << " "
+             << "price:" << GetPrice() << " "
+             << "neutered:" << boolalpha << neutered << " "
+             << "vaccinaed:" << boolalpha << vaccinaed << endl
+             << endl;
+    }
+
+    bool GetNeutered()
+    {
+        return neutered;
+    }
+
+    bool GetVaccinaed()
+    {
+        return vaccinaed;
+    }
+};
+
+class Dog : public Pet
+{
+private:
+    bool neutered;
+    bool vaccinaed;
+
+public:
+    Dog(const string &storeId, const string &breed, const string &age, const string &colour, const string &origin, const string &price, bool neutered, bool vaccinaed) : Pet(storeId, breed, age, colour, origin, price), neutered(neutered), vaccinaed(vaccinaed)
+    {
+        cout << "The dog is created" << endl;
+    }
+
+    string Type()
+    {
+        return "Dog";
+    }
+
+    void Show()
+    {
+        cout << Type() << " "
+             << "storeId:" << GetStoreId() << " "
+             << "breed:" << GetBreed() << " "
+             << "age:" << GetAge() << " "
+             << "colour:" << GetColour() << " "
+             << "price:" << GetPrice() << " "
+             << "neutered:" << boolalpha << neutered << " "
+             << "vaccinaed:" << boolalpha << vaccinaed << endl
+             << endl;
+    }
+
+    bool GetNeutered()
+    {
+        return neutered;
+    }
+
+    bool GetVaccinaed()
+    {
+        return vaccinaed;
+    }
+};
+
+class Bird : public Pet
+{
+private:
+    bool licenced;
+
+public:
+    Bird(const string &storeId, const string &breed, const string &age, const string &colour, const string &origin, const string &price, bool licenced) : Pet(storeId, breed, age, colour, origin, price), licenced(licenced)
+    {
+        cout << "The bird is created" << endl;
+    }
+
+    string Type()
+    {
+        return "Bird";
+    }
+
+    void Show()
+    {
+        cout << Type() << " "
+             << "storeId:" << GetStoreId() << " "
+             << "breed:" << GetBreed() << " "
+             << "age:" << GetAge() << " "
+             << "colour:" << GetColour() << " "
+             << "price:" << GetPrice() << " "
+             << "licenced:" << boolalpha << licenced << endl
+             << endl;
+    }
+
+    bool GetLicenced()
+    {
+        return licenced;
+    }
+};
+
+// struct TreeNode
+// {
+//     int Id;
+//     TreeNode *lparent, *rparent;
+// };
+
+class Shop
+{
+private:
+    vector<Pet *> pets;
+    // vector<TreeNode *> family;
+
+public:
+    Shop(){};
+
+    void AddPets(Pet *&p)
+    {
+        pets.push_back(p);
+    }
+
+    int RemovePets(string id)
+    {
+        int flag = 0;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetStoreId() == id)
+            {
+                pets.erase(pets.begin() + i);
+                flag = 1;
+            }
+        }
+        return flag;
+    }
+
+    void SearchByType(string type)
+    {
+        int flag = 0;
+        int count = 1;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).Type() == type)
+            {
+                cout << count++ << ". ";
+                (*pets[i]).Show();
+                flag = 1;
+            }
+        }
+        if (!flag)
+            cout << "There is no such pets.\n"
+                 << endl;
+    }
+
+    int SearchById(string id)
+    {
+        int flag = 0;
+        int count = 1;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetStoreId() == id)
+            {
+                cout << "\n"
+                     << count++ << ". ";
+                (*pets[i]).Show();
+                flag = 1;
+            }
+        }
+        return flag;
+    }
+
+    void SearchByPrice(string minprice, string maxprice)
+    {
+        int flag = 0;
+        int count = 1;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetPrice() >= minprice && (*pets[i]).GetPrice() <= maxprice)
+            {
+                cout << count++ << ". ";
+                (*pets[i]).Show();
+                flag = 1;
+            }
+        }
+        if (!flag)
+            cout << "There is no such pets.\n"
+                 << endl;
+    }
+
+    void SearchByDetail(string type, bool detail1, bool detail2)
+    {
+        int flag = 0;
+        int count = 1;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).Type() == type && (*pets[i]).GetNeutered() == detail1 && (*pets[i]).GetVaccinaed() == detail2)
+            {
+                cout << count++ << ". ";
+                (*pets[i]).Show();
+                flag = 1;
+            }
+        }
+        if (!flag)
+            cout << "There is no such pets.\n"
+                 << endl;
+    }
+
+    void SearchByDetail(string type, bool detail1)
+    {
+        int flag = 0;
+        int count = 1;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).Type() == type && (*pets[i]).GetLicenced() == detail1)
+            {
+                cout << count++ << ". ";
+                (*pets[i]).Show();
+                flag = 1;
+            }
+        }
+        if (!flag)
+            cout << "There is no such pets.\n"
+                 << endl;
+    }
+
+    void DisplayAll()
+    {
+        int flag = 0;
+        int count = 1;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            cout << count++ << ": ";
+            (*pets[i]).Show();
+            flag = 1;
+        }
+        if (!flag)
+            cout << "There is no pets.\n"
+                 << endl;
+    }
+
+    void SavePets()
+    {
+        ofstream outfile;
+        outfile.open("pets.dat");
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).Type() == "Bird")
+            {
+                outfile << "Bird" << ' ' << (*pets[i]).GetStoreId() << ' ' << (*pets[i]).GetBreed() << ' ' << (*pets[i]).GetAge() << ' ' << (*pets[i]).GetColour()
+                        << ' ' << (*pets[i]).GetOrigin() << ' ' << (*pets[i]).GetPrice() << ' ' << (*pets[i]).GetLicenced() << endl;
+            }
+            else if ((*pets[i]).Type() == "Cat")
+            {
+                outfile << "Cat" << ' ' << (*pets[i]).GetStoreId() << ' ' << (*pets[i]).GetBreed() << ' ' << (*pets[i]).GetAge() << ' ' << (*pets[i]).GetColour()
+                        << ' ' << (*pets[i]).GetOrigin() << ' ' << (*pets[i]).GetPrice() << ' ' << (*pets[i]).GetNeutered() << ' ' << (*pets[i]).GetVaccinaed() << endl;
+            }
+            else if ((*pets[i]).Type() == "Dog")
+            {
+                outfile << "Dog" << ' ' << (*pets[i]).GetStoreId() << ' ' << (*pets[i]).GetBreed() << ' ' << (*pets[i]).GetAge() << ' ' << (*pets[i]).GetColour()
+                        << ' ' << (*pets[i]).GetOrigin() << ' ' << (*pets[i]).GetPrice() << ' ' << (*pets[i]).GetNeutered() << ' ' << (*pets[i]).GetVaccinaed() << endl;
+            }
+        }
+        outfile.close();
+    }
+
+    void SplitString(const string &s, vector<string> &v, const string &c)
+    {
+        string::size_type pos1, pos2;
+        pos2 = s.find(c);
+        pos1 = 0;
+        while (string::npos != pos2)
+        {
+            v.push_back(s.substr(pos1, pos2 - pos1));
+
+            pos1 = pos2 + c.size();
+            pos2 = s.find(c, pos1);
+        }
+        if (pos1 != s.length())
+            v.push_back(s.substr(pos1));
+    }
+
+    void ReadPets()
+    {
+        ifstream infile;
+        infile.open("pets.dat");
+        string line;
+        vector<string> lines;
+        if (infile)
+        {
+            while (getline(infile, line))
+            {
+                lines.push_back(line);
+            }
+        }
+        else
+            cout << "There is no such file." << endl;
+        // cout<<lines[0];
+        // cout<<lines[1];
+        for (int i = 0; i < lines.size(); i++)
+        {
+            vector<string> p;
+            SplitString(lines[i], p, " ");
+            if (p[0] == "Cat")
+            {
+                Pet *temp = new Cat(p[1], p[2], p[3], p[4], p[5], p[6], atoi(p[7].c_str()), atoi(p[8].c_str()));
+                AddPets(temp);
+                // Can't delete here.
+            }
+            else if (p[0] == "Dog")
+            {
+                Pet *temp = new Dog(p[1], p[2], p[3], p[4], p[5], p[6], atoi(p[7].c_str()), atoi(p[8].c_str()));
+                AddPets(temp);
+            }
+            else if (p[0] == "Bird")
+            {
+                Pet *temp = new Bird(p[1], p[2], p[3], p[4], p[5], p[6], atoi(p[7].c_str()));
+                AddPets(temp);
+            }
+            // 需要保存的指针必须使用new来建立
+            // 在if语句中直接创建的对象在if语句结束后即被释放（在栈中），首地址会在下一个对象创建时重复使用
+        }
+    }
+
+    void AddFamily(string id1, string id2)
+    {
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetStoreId() == id1)
+            {
+                (*pets[i]).SetMotherId(id2);
+            }
+        }
+    }
+
+    int ShowFamily()
+    {
+        int flag = 0;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetMotherId() != " ")
+            {
+                cout << (*pets[i]).GetStoreId() << "'s mother is " << (*pets[i]).GetMotherId() << "." << endl;
+                flag = 1;
+            }
+        }
+        return flag;
+    }
+
+    void SaveFamily()
+    {
+        ofstream outfile;
+        outfile.open("family.dat");
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetMotherId() != " ")
+                outfile << (*pets[i]).GetStoreId() << " " << (*pets[i]).GetMotherId() << endl;
+        }
+        outfile.close();
+    }
+
+    void ReadFamily()
+    {
+        ifstream infile;
+        infile.open("family.dat");
+        string line;
+        vector<string> lines;
+        if (infile)
+        {
+            while (getline(infile, line))
+            {
+                lines.push_back(line);
+            }
+        }
+        else
+            cout << "There is no such file." << endl;
+        for (int i = 0; i < lines.size(); i++)
+        {
+            vector<string> p;
+            SplitString(lines[i], p, " ");
+            for (int j = 0; j < pets.size(); j++)
+            {
+                if ((*pets[j]).GetStoreId() == p[0])
+                    (*pets[j]).SetMotherId(p[1]);
+            }
+        }
+    }
+
+    int SearchBaby(string id)
+    {
+        int flag = 0;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetMotherId() == id)
+                cout << id << "'s baby is " << (*pets[i]).GetStoreId() << endl;
+            flag = 1;
+        }
+        return flag;
+    }
+
+    int SearchMother(string id)
+    {
+        int flag = 0;
+        for (int i = 0; i < pets.size(); i++)
+        {
+            if ((*pets[i]).GetStoreId() == id&&(*pets[i]).GetMotherId() != " ")
+            {
+                cout << id << "'s mother is " << (*pets[i]).GetMotherId() << endl;
+                flag = 1;
+            }
+        }
+        return flag;
+    }
+};
